4c83d21a6dde9f094632662f651ca6853aab7524
Abdul Rafay <arafay696@gmail.com>
2016-09-07 23:49:34 +0500

    Not Found Exception - Solved

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,14 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Http\Controllers\Client\CommonFunction;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Redirect;
+use Session;
+use DB;
+use View;
 
 class Handler extends ExceptionHandler
 {
+    use CommonFunction;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -42,10 +48,34 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if ($e instanceof ModelNotFoundException) {
-            $e = new NotFoundHttpException($e->getMessage(), $e);
+        if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+            Session::flash('globalErrMsg','Sorry, the page you are looking for could not be found.');
+            Session::flash('alert-class', 'alert-danger');
+
+            $reviews = DB::table('customer_reviews AS cr')
+                ->select('c.Name', 'cr.Review','c.UserImg')
+                ->join('customers as c', 'c.ID', '=', 'cr.CustomerID')
+                ->get();
+
+            $data = array(
+                'products' => array(),
+                'reviews' => $reviews
+            );
+
+            $itemSelected = $this->getCartData();
+            $ShareData = array(
+                'itemSelected' => $itemSelected
+            );
+            View::share('ShareData', $ShareData);
+            return response(view('client.home',$data), 404);
         }
 
         return parent::render($request, $e);
+
+        /*if ($e instanceof ModelNotFoundException) {
+            $e = new NotFoundHttpException($e->getMessage(), $e);
+        }
+
+        return parent::render($request, $e);*/
     }
 }
